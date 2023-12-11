46843e12b39ecf52f33ba7b657cfdc1f167096b9
Rick South <rick@natamus.com>
2023-12-11 23:48:13 +0100

    Villager Names source code for 1.20.4.

---
+++ NeoForge/src/main/java/com/natamus/villagernames/ModNeoForge.java
@@ -0,0 +1,36 @@
+package com.natamus.villagernames;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.villagernames.neoforge.config.IntegrateNeoForgeConfig;
+import com.natamus.villagernames.neoforge.events.NeoForgeVillagerEvent;
+import com.natamus.villagernames.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateNeoForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeVillagerEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
