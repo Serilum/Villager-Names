b237608d41c7658154d53e8a08a81d54e1f51006
Rick South <rick@natamus.com>
2023-12-11 23:47:54 +0100

    Villager Names source code for 1.20.0.

---
+++ Forge/src/main/java/com/natamus/villagernames/ModForge.java
@@ -0,0 +1,36 @@
+package com.natamus.villagernames;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.villagernames.forge.config.IntegrateForgeConfig;
+import com.natamus.villagernames.forge.events.ForgeVillagerEvent;
+import com.natamus.villagernames.util.Reference;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		MinecraftForge.EVENT_BUS.register(new ForgeVillagerEvent());
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
