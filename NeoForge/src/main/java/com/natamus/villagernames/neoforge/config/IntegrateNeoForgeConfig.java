c245a07e36f089c305e0dfe1b7e0d533d0266c7b
Rick South <rick@natamus.com>
2024-04-28 01:20:32 +0200

    Villager Names source code for 1.20.5.

---
+++ NeoForge/src/main/java/com/natamus/villagernames/neoforge/config/IntegrateNeoForgeConfig.java
@@ -0,0 +1,14 @@
+package com.natamus.villagernames.neoforge.config;
+
+import com.natamus.collective.config.DuskConfig;
+import com.natamus.villagernames.util.Reference;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.fml.ModLoadingContext;
+
+public class IntegrateNeoForgeConfig {
+	public static void registerScreen(ModLoadingContext modLoadingContext) {
+		modLoadingContext.registerExtensionPoint(IConfigScreenFactory.class, () -> (mc, screen) -> {
+			return DuskConfig.DuskConfigScreen.getScreen(screen, Reference.MOD_ID);
+		});
+	}
+}
---
+++ NeoForge/src/main/java/com/natamus/villagernames/neoforge/events/NeoForgeVillagerEvent.java
@@ -0,0 +1,20 @@
+package com.natamus.villagernames.neoforge.events;
+
+import com.natamus.villagernames.events.VillagerEvent;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeVillagerEvent {
+	@SubscribeEvent
+	public static void onSpawn(EntityJoinLevelEvent e) {
+		VillagerEvent.onSpawn(e.getLevel(), e.getEntity());
+	}
+
+	@SubscribeEvent
+	public static void onPlayerInteract(PlayerInteractEvent.EntityInteract e) {
+		VillagerEvent.onVillagerInteract(e.getEntity(), e.getLevel(), e.getHand(), e.getTarget(), null);
+	}
+}
