13a3d34a855e27d83a1042f1b82bb7660094c4fd
Rick South <rick@natamus.com>
2024-06-28 11:39:42 +0200

    (v8.0) Added commands to unname, name and rename loaded villagers. [+1]

--- Common/src/main/java/com/natamus/villagernames/util/Reference.java
+++ Common/src/main/java/com/natamus/villagernames/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.villagernames.util;
 public class Reference {
 	public static final String MOD_ID = "villagernames";
 	public static final String NAME = "Villager Names";
-	public static final String VERSION = "7.4";
+	public static final String VERSION = "8.0";
 	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
 }
--- Forge/src/main/java/com/natamus/villagernames/forge/events/ForgeVillagerEvent.java
+++ Forge/src/main/java/com/natamus/villagernames/forge/events/ForgeVillagerEvent.java
@@ -1,6 +1,8 @@
 package com.natamus.villagernames.forge.events;
 
+import com.natamus.villagernames.cmds.CommandVillagernames;
 import com.natamus.villagernames.events.VillagerEvent;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -17,4 +19,9 @@ public class ForgeVillagerEvent {
 	public void onPlayerInteract(PlayerInteractEvent.EntityInteract e) {
 		VillagerEvent.onVillagerInteract(e.getEntity(), e.getLevel(), e.getHand(), e.getTarget(), null);
 	}
+
+    @SubscribeEvent
+    public void registerCommands(RegisterCommandsEvent e) {
+    	CommandVillagernames.register(e.getDispatcher());
+    }
 }
