fcf5fc2f955eef434fa112deb711500d433d5b3f
Rick South <rick@natamus.com>
2024-07-28 07:10:08 +0200

    (v8.1) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/villagernames/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/villagernames/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.villagernames;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.villagernames.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.villagernames.neoforge.events.NeoForgeVillagerEvent;
 import com.natamus.villagernames.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
