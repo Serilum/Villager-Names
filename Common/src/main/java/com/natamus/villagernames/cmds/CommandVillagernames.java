8899619a45683b63525e70a9e1dbc95d66e909f8
Rick South <38502394+ricksouth@users.noreply.github.com>
2026-06-07 17:54:45 +0200

    (v8.5) Updated the mod's text to use Collective's new translation system, which adds many languages alongside English! [+1]

--- Common/src/main/java/com/natamus/villagernames/cmds/CommandVillagernames.java
+++ Common/src/main/java/com/natamus/villagernames/cmds/CommandVillagernames.java
@@ -1,4 +1,5 @@
 package com.natamus.villagernames.cmds;
+import com.natamus.villagernames.util.Reference;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.natamus.collective.functions.MessageFunctions;
@@ -22,7 +23,7 @@ public class CommandVillagernames {
 
 				int count = Util.unnameLoadedVillagers(source.getLevel());
 
-				MessageFunctions.sendMessage(source, "The names of " + count + " loaded villagers have been removed.", ChatFormatting.DARK_GREEN);
+				MessageFunctions.sendTranslatableMessage(source, "collective.villagernames.message.namesloadedvillagers", ChatFormatting.DARK_GREEN, count);
 				return 1;
 			}))
 			.then(Commands.literal("name")
@@ -31,7 +32,7 @@ public class CommandVillagernames {
 
 				int count = Util.nameLoadedVillagers(source.getLevel());
 
-				MessageFunctions.sendMessage(source, count + " loaded villagers without a name have been named.", ChatFormatting.DARK_GREEN);
+				MessageFunctions.sendTranslatableMessage(source, "collective.villagernames.message.loadedvillagerswithout", ChatFormatting.DARK_GREEN, count);
 				return 1;
 			}))
 			.then(Commands.literal("rename")
@@ -40,19 +41,19 @@ public class CommandVillagernames {
 
 				int count = Util.renameLoadedVillagers(source.getLevel());
 
-				MessageFunctions.sendMessage(source, count + " loaded villagers have been renamed.", ChatFormatting.DARK_GREEN);
+				MessageFunctions.sendTranslatableMessage(source, "collective.villagernames.message.loadedvillagersrenamed", ChatFormatting.DARK_GREEN, count);
 				return 1;
 			}))
 		);
 	}
 
 	public static void sendUsage(CommandSourceStack source) {
-		MessageFunctions.sendMessage(source, "--- Villager Names Commands Usage ---", ChatFormatting.DARK_GREEN, true);
+		MessageFunctions.sendTranslatableMessage(source, "collective.shared.message.commandsusage", true, ChatFormatting.DARK_GREEN, Reference.NAME);
 		MessageFunctions.sendMessage(source, " /villagernames unname", ChatFormatting.DARK_GREEN);
-		MessageFunctions.sendMessage(source, "  Removes the names of all loaded villagers.", ChatFormatting.DARK_GRAY);
+		MessageFunctions.sendTranslatableMessage(source, "  ", "collective.villagernames.message.removesnamesloaded", ChatFormatting.DARK_GRAY);
 		MessageFunctions.sendMessage(source, " /villagernames name", ChatFormatting.DARK_GREEN);
-		MessageFunctions.sendMessage(source, "  Gives all unnamed loaded villagers a name.", ChatFormatting.DARK_GRAY);
+		MessageFunctions.sendTranslatableMessage(source, "  ", "collective.villagernames.message.givesunnamedloaded", ChatFormatting.DARK_GRAY);
 		MessageFunctions.sendMessage(source, " /villagernames rename", ChatFormatting.DARK_GREEN);
-		MessageFunctions.sendMessage(source, "  Gives all loaded villagers a new random name.", ChatFormatting.DARK_GRAY);
+		MessageFunctions.sendTranslatableMessage(source, "  ", "collective.villagernames.message.givesloadedvillagers", ChatFormatting.DARK_GRAY);
 	}
 }
