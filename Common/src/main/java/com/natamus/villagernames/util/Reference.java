94f788b5eba61048d7e4bfcbeaceaafd03f40df8
Rick South <rick@natamus.com>
2026-06-18 13:08:17 +0200

    Updated Villager Names to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/villagernames/util/Reference.java
+++ Common/src/main/java/com/natamus/villagernames/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "villagernames";
 	public static final String NAME = "Villager Names";
 	public static final String VERSION = "8.5";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
